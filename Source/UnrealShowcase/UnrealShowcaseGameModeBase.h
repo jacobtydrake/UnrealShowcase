a5c8e6735ffc949cf4aaf3d79cd0960976fd4375
Jacob Drake <57561539+Citrusalt@users.noreply.github.com>
2023-09-30 09:25:21 -0500

    Project Creation

---
+++ Source/UnrealShowcase/UnrealShowcaseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealShowcaseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSHOWCASE_API AUnrealShowcaseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
